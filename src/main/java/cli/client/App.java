821320b84715935ccc02efb894760e9f9d1a1b64
Romoli Gabriele <gh05romoli@gmail.com>
2021-09-27 16:44:16 +0200

    cli quasi termianto

--- src/main/java/cli/client/App.java
+++ src/main/java/cli/client/App.java
@@ -1,11 +1,14 @@
 package cli.client;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        
+        client client1 = new client();
+        client1.connetti();
+        client1.comunica();
     }
 }
--- src/main/java/cli/client/client.java
+++ src/main/java/cli/client/client.java
@@ -6,15 +6,40 @@ import java.net.*;
 public class client {
     String nomeServer = "server1";
     int portaServer = 6789;
+    Socket socket;
+    BufferedReader tastiera;
+    String StringUser;
+    String StringReply;
     DataInputStream in;
     DataOutputStream out;   
 
     protected Socket connetti() throws IOException
     {
-        Socket socket = new Socket(nomeServer, portaServer);
-        out = new DataOutputStream(socket.getOutputStream());
-        in = new DataInputStream(socket.getInputStream());
-        
+        try{
+            tastiera = new BufferedReader( new InputStreamReader(System.in));
+            socket = new Socket(nomeServer, portaServer);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+        }catch (UnknownHostException a){
+            System.err.println("");
+        }catch (Exception e){
+            System.out.println("");
+            System.exit(1);
+        }
         return socket;
+    }
+    public void comunica(){
+        try{
+            System.out.println("4 Inserisci stringa da modificare: "+'\n');
+            StringUser = tastiera.readLine();
+            System.out.println("5 Invio stringa al server e attendo...");
+            out.writeBytes(StringUser+'\n');
+            StringReply = in.readLine();
+            System.out.println("... risposta dal server "+ '\n'+StringReply);
+            System.out.println("9 CLI: terminata belaborazione chiusura connessione");
+            socket.close();
+        }catch (Exception e){
+            System.exit(1);
+        }
     }
 }
